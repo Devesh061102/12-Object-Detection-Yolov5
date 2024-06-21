683f040378d2580367746343722de321eedd01a5
Devesh061102 <deveshnegi10nov@gmail.com>
2024-06-21 09:35:49 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Devesh Singh',
+    author_email= 'deveshnegi10nov@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
